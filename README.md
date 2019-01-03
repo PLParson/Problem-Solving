8a501bbc45638b1d32a0166e49e1652520acdaa0
PLParson <plparson01@gmail.com>
2019-01-03 15:01:38 +0000

    Solution for A Very Big Sum - https://www.hackerrank.com/challenges/a-very-big-sum/problem

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ The solution is located in src/Problem../Solution.Java
 Problem 1: Solve Me First
 Problem 2: Simple Array Sum
 Problem 3: Compare the Triplets
+Problem 4: A Very Big Sum
---
+++ src/Problem4/Solution.java
@@ -0,0 +1,50 @@
+package Problem4;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * https://www.hackerrank.com/challenges/a-very-big-sum/problem
+ */
+public class Solution {
+    // Complete the aVeryBigSum function below.
+    static long aVeryBigSum(long[] ar) {
+        long sum = 0;
+        for(long l : ar){
+            sum+=l;
+        }
+        return sum;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        //BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int arCount = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        long[] ar = new long[arCount];
+
+        String[] arItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < arCount; i++) {
+            long arItem = Long.parseLong(arItems[i]);
+            ar[i] = arItem;
+        }
+
+        long result = aVeryBigSum(ar);
+
+        //bufferedWriter.write(String.valueOf(result));
+        System.out.println(result);
+        //bufferedWriter.newLine();
+
+        //bufferedWriter.close();
+
+        scanner.close();
+    }
+}
